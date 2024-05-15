8af1585d081b61594d259d29b34e18cbb64175be
Lucky <lucas.beliz@hotmail.com>
2024-05-15 02:58:06 -0300

    actualizo la view de RegistrarUsuario

--- Aplicaciones/GestionUsuarios/RegistrarUsuario/views.py
+++ Aplicaciones/GestionUsuarios/RegistrarUsuario/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import UsuarioRegistradoForm
-from django.contrib.auth import login #para crear cookie de logueo
 
 def registro(request):
     if request.method == 'POST': #si aprieto registrar...
         form = UsuarioRegistradoForm(request.POST) #creo un formulario con los datos recibidos
         if form.is_valid(): #compruebo validaciones def clean_... hechas en el forms
             usuario = form.save()
-            login(request, usuario)
             return redirect('exito_registro')
     else:
         form = UsuarioRegistradoForm() #retorno formulario con campos fallidos vacíos
